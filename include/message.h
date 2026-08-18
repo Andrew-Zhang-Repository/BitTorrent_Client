b6b5d75f137c4d960325e0a5b5b7506f0ff6171d
andrew zhang <andrewzhang0313@icloud.com>
2026-08-18 17:24:14 +1200

    Refactored code and added pipelined requests to increase speeds

--- include/message.h
+++ include/message.h
@@ -20,14 +20,26 @@
 #include <vector>
 #include "../include/torrent_engine.h"
 #include "../include/peer.h"
- #include <fstream>
- #include <cstdlib>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
 #pragma once
 struct DownloadState;
+struct PendingRequest;
 struct DownloadState {
     uint32_t current_piece_index = 0;
     uint32_t current_block_offset = 0;
+    uint32_t bytes_received = 0;
     std::string piece_buffer = "";
+    std::map<uint32_t, std::string> dict;
+    std::vector<PendingRequest> pending_requests;
+    uint32_t pipeline_depth = 10;
+};
+
+struct PendingRequest {
+    uint32_t piece_index;
+    uint32_t block_offset;
+    uint32_t block_length;
 };
 
 
@@ -36,6 +48,6 @@ std::string recv_exact(int n,int sock);
 message_code get_code(int id);
 void send_interested(int sock);
 void run_message_loop(int sock, TorrentFile tf);
-void send_request(int sock, uint32_t piece_index, uint32_t block_offset, long long block_length);
+PendingRequest send_request(int sock, uint32_t piece_index, uint32_t block_offset, long long block_length);
 void retrieve(DownloadState &ds, TorrentFile tf ,const std::string& payload, int sock, std::ofstream &outfile);
 void handle_message(int sock, message_code id, const std::string& payload, DownloadState &ds, TorrentFile tf, std::ofstream &outfile);
--- src/message.cpp
+++ src/message.cpp
@@ -1,8 +1,10 @@
 
 #include "../include/message.h"
 #include <arpa/inet.h> 
+#include <map> 
 // Standard BitTorrent block size is always 16 KB
 const uint32_t STANDARD_BLOCK_SIZE = 16384;
+
 
 
 void handle_message(int sock, message_code id, const std::string& payload, DownloadState &ds, TorrentFile tf, std::ofstream &outfile) {
@@ -17,7 +19,20 @@ void handle_message(int sock, message_code id, const std::string& payload, Downl
             ds.current_piece_index = 0;
             ds.current_block_offset = 0;
             ds.piece_buffer.clear();
-            send_request(sock, ds.current_piece_index, ds.current_block_offset,STANDARD_BLOCK_SIZE);
+            ds.piece_buffer.assign(tf.piece_length, '\0'); 
+            ds.pending_requests.clear();
+            for (int i = 0; i < 10; i++) {
+                if (ds.current_block_offset >= tf.piece_length) break; 
+                
+                uint32_t remaining = tf.piece_length - ds.current_block_offset;
+                uint32_t req_size = (remaining < STANDARD_BLOCK_SIZE) ? remaining : STANDARD_BLOCK_SIZE;
+
+                PendingRequest request = send_request(sock, ds.current_piece_index, ds.current_block_offset, req_size);
+                ds.pending_requests.push_back(request);
+                
+                ds.current_block_offset += req_size; 
+            }
+            
             break;
         
         case message_code::BITFIELD:
@@ -39,14 +54,36 @@ void handle_message(int sock, message_code id, const std::string& payload, Downl
 
 void retrieve(DownloadState &ds, TorrentFile tf ,const std::string& payload, int sock, std::ofstream &outfile){
     std::string block_data = payload.substr(8);
-    ds.piece_buffer += block_data;
-    ds.current_block_offset += block_data.size();
+
+    uint32_t index = (static_cast<uint32_t>(static_cast<unsigned char>(payload[0])) << 24) |
+    (static_cast<uint32_t>(static_cast<unsigned char>(payload[1])) << 16) |
+    (static_cast<uint32_t>(static_cast<unsigned char>(payload[2])) << 8)  |
+    static_cast<uint32_t>(static_cast<unsigned char>(payload[3]));
+
+    uint32_t offset = (static_cast<uint32_t>(static_cast<unsigned char>(payload[4])) << 24) |
+    (static_cast<uint32_t>(static_cast<unsigned char>(payload[5])) << 16) |
+    (static_cast<uint32_t>(static_cast<unsigned char>(payload[6])) << 8)  |
+    static_cast<uint32_t>(static_cast<unsigned char>(payload[7]));
+
+
+    // Get correct request from vector
+    for (auto i = ds.pending_requests.begin();i != ds.pending_requests.end(); ++i){
+        if (i->block_offset == offset && i->piece_index == index){
+            ds.pending_requests.erase(i); 
+            break;
+        }
+    }
+
+
+    std::copy(block_data.begin(), block_data.end(), ds.piece_buffer.begin() + offset);
+    ds.bytes_received += block_data.size();
+
 
 
     uint32_t total_pieces = tf.pieces.length() / 20;
     uint32_t current_piece_size = tf.piece_length;
 
-    if (ds.current_piece_index == total_pieces - 1) {
+    if (index == total_pieces - 1) {
         current_piece_size = tf.length % tf.piece_length;
         if (current_piece_size == 0){
             current_piece_size = tf.piece_length;
@@ -55,36 +92,29 @@ void retrieve(DownloadState &ds, TorrentFile tf ,const std::string& payload, int
     }
 
 
-    if (ds.current_block_offset < current_piece_size){
-        uint32_t remaining = current_piece_size - ds.current_block_offset;
-        uint32_t next_request_size;
-
-        if (remaining < STANDARD_BLOCK_SIZE){
-            next_request_size = remaining;
-        }
-        else{
-            next_request_size = STANDARD_BLOCK_SIZE;
-        }
-        
-        send_request(sock, ds.current_piece_index, ds.current_block_offset, next_request_size);
-
-    }
-    else{
-
-        std::string expected_hash = tf.pieces.substr(ds.current_piece_index * 20, 20);
+    if (ds.bytes_received == current_piece_size){
+        std::string expected_hash = tf.pieces.substr(index * 20, 20);
         std::string actual_hash = calculateSHA1(ds.piece_buffer);
-        
+
         if (actual_hash == expected_hash) {
             std::cout << "[SUCCESS] Hash match! Writing to disk" << std::endl;
             
             
-            outfile.seekp(ds.current_piece_index * tf.piece_length);
+            outfile.seekp(index * tf.piece_length);
             outfile.write(ds.piece_buffer.data(), ds.piece_buffer.size());
             outfile.flush();
             
             ds.current_piece_index++;
             ds.current_block_offset = 0;
+            ds.bytes_received = 0;
             ds.piece_buffer.clear();
+
+            uint32_t refill_piece_size = tf.piece_length;
+            if (ds.current_piece_index == total_pieces - 1) {
+                refill_piece_size = tf.length % tf.piece_length;
+                if (refill_piece_size == 0) refill_piece_size = tf.piece_length;
+            }
+            ds.piece_buffer.assign(refill_piece_size, '\0');
             
             uint32_t total_pieces = tf.pieces.length() / 20;
         
@@ -95,33 +125,33 @@ void retrieve(DownloadState &ds, TorrentFile tf ,const std::string& payload, int
                 close(sock);
                 exit(0); 
             }
-            
-            uint32_t next_piece_size = tf.piece_length;
-            if (ds.current_piece_index == total_pieces - 1) {
-                next_piece_size = tf.length % tf.piece_length;
-                if (next_piece_size == 0) next_piece_size = tf.piece_length;
-            }
-            uint32_t req_size = (next_piece_size < STANDARD_BLOCK_SIZE) ? next_piece_size : STANDARD_BLOCK_SIZE;
-            
-            send_request(sock, ds.current_piece_index, ds.current_block_offset, req_size);
         }
         else{
-            std::cerr << "[ERROR] Hash mismatch! Corrupted piece. Retrying..." << std::endl;
             ds.current_block_offset = 0;
+            ds.bytes_received = 0;
             ds.piece_buffer.clear();
-
-            uint32_t retry_piece_size = tf.piece_length;
-            if (ds.current_piece_index == total_pieces - 1) {
-                retry_piece_size = tf.length % tf.piece_length;
-                if (retry_piece_size == 0) retry_piece_size = tf.piece_length;
-            }
-            uint32_t req_size = (retry_piece_size < STANDARD_BLOCK_SIZE) ? retry_piece_size : STANDARD_BLOCK_SIZE;
-
-            send_request(sock, ds.current_piece_index, ds.current_block_offset, req_size);
+            ds.piece_buffer.assign(current_piece_size, '\0');
         }
 
     }
+
+    uint32_t refill_piece_size = tf.piece_length;
+    if (ds.current_piece_index == total_pieces - 1) {
+        refill_piece_size = tf.length % tf.piece_length;
+        if (refill_piece_size == 0) refill_piece_size = tf.piece_length;
+    }
+    while (ds.pending_requests.size() < 10 && ds.current_block_offset < refill_piece_size){
+        uint32_t remaining =  refill_piece_size - ds.current_block_offset;
+        uint32_t req_size = std::min(STANDARD_BLOCK_SIZE, remaining);
+        ds.pending_requests.push_back(send_request(sock,ds.current_piece_index, ds.current_block_offset, req_size));
+        ds.current_block_offset += req_size;
+    }
+
+    
 }
+    
+    
+
 
 
 void send_interested(int sock) {
@@ -155,8 +185,9 @@ std::string recv_exact(int n,int sock){
     return peer_response;
 }
 
-void send_request(int sock, uint32_t piece_index, uint32_t block_offset, long long block_length) {
-   
+PendingRequest send_request(int sock, uint32_t piece_index, uint32_t block_offset, long long block_length) {
+    
+    PendingRequest request;
     std::vector<uint8_t> send_vect(17);
     send_vect.reserve(17);
     uint32_t net_len = htonl(13);
@@ -175,6 +206,12 @@ void send_request(int sock, uint32_t piece_index, uint32_t block_offset, long lo
 
     send(sock, send_vect.data(), send_vect.size(), 0);
     std::cout << "-> Sent REQUEST for Piece: " << piece_index << ", Offset: " << block_offset << std::endl;
+
+    request.piece_index = piece_index;
+    request.block_offset = block_offset;
+    request.block_length = block_length;
+
+    return request;
 }
 
 void run_message_loop(int sock, TorrentFile tf) {
